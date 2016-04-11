22f666400cbe362ae07f85ad3df27d574b82c3cc
zhangkong <602488225@qq.com>
2016-04-11 13:18:23 +0800

    正则相关帮助类、单元测试

---
+++ UnitTest/Regex/RegexHelperTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZK.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZK.Utility.Tests
+{
+    [TestClass()]
+    public class RegexHelperTests
+    {
+        [TestMethod()]
+        public void IsMobileTest()
+        {
+            var input = "15827395018";
+            var r=RegexHelper.IsMobile(input);
+            Assert.IsTrue(r);
+        }
+
+        [TestMethod()]
+        public void IsEmailTest()
+        {
+            var input = "niubileme@hotmail.com";
+            var r = RegexHelper.IsEmail(input);
+            Assert.IsTrue(r);
+        }
+
+        [TestMethod()]
+        public void IsChineseTest()
+        {
+            var input = "这是汉字";
+            var r = RegexHelper.IsChinese(input);
+            Assert.IsTrue(r);
+        }
+
+        [TestMethod()]
+        public void IsIPAddressTest()
+        {
+            var input = "127.0.0.1";
+            var r = RegexHelper.IsIPAddress(input);
+            Assert.IsTrue(r);
+        }
+    }
+}
