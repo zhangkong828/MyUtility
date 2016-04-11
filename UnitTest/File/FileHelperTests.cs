51eb4dd45d05e7f733d8e60c4405b838fbe18f7d
zhangkong <602488225@qq.com>
2016-04-11 13:07:37 +0800

    文件相关帮助类

---
+++ UnitTest/File/FileHelperTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZK.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZK.Utility.Tests
+{
+    [TestClass()]
+    public class FileHelperTests
+    {
+        [TestMethod()]
+        public void GetAbsolutePathTest()
+        {
+            var a = @"C:\log.log";
+            var b=FileHelper.GetAbsolutePath(a);
+            Assert.AreEqual(a,b);
+        }
+    }
+}
